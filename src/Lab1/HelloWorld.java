5d77c30be160bb6354c130449fb765d626426bc1
Jar <natthanan.s@rmutsvmail.com>
2020-07-14 17:53:56 +0700

    add code 2 linea

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
     public static void main(String[] args) {
       System.out.println("Hello World !!!");
       System.out.println("NATTHANAN");
+      System.out.println("JaJar");
+      System.out.println("Tar");
+
+
 
     }
 }
