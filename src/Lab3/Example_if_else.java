dfde14a135505dae2752f5ddc25312658f032c78
LabCom_MT-4 <natthanan.s@rmutsvmail.com>
2020-08-11 18:43:41 +0700

    Lab 3

---
+++ src/Lab3/Example_if_else.java
@@ -0,0 +1,25 @@
+package Lab3;
+
+import java.util.Scanner;
+
+public class Example_if_else {
+    public static void main(String[] args) {
+
+        Scanner sc  = new Scanner(System.in);
+
+
+        System.out.println("Enter integer 1: ");
+        int x = sc.nextInt();
+        System.out.println("Enter integer 2: ");
+        int y = sc.nextInt();
+
+
+        //condition
+        if (x > y){
+            System.out.println(x+"grater than"+y);
+        } else {
+            System.out.println(x+" less than"+y);
+        }
+        System.out.println("Good Bye.");
+    }//main
+}
