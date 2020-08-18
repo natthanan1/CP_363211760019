67927528a35058237705f9b78251ef5c760766c6
LabCom_MT-4 <natthanan.s@rmutsvmail.com>
2020-08-18 16:36:33 +0700

    Lab 3

---
+++ src/Exersice/Exercise_Lad3_2.java
@@ -0,0 +1,19 @@
+package Exersice;
+
+public class Exercise_Lad3_2 {
+
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <=10; i++) {// loop row
+            for (int j = 1; j <= i; j++) {// loop column
+                System.out.print(j);
+            }//j
+            System.out.println();
+        }//i
+
+
+
+
+    }
+}
